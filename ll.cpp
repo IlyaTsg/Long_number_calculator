9279a9323f28cd58f0d31eab5d7d3fdd67b1cd45
Ilya.Tsygankov <saw44947@gmail.com>
2021-04-15 01:02:26 +0300

    Add all constructors

--- ll.cpp
+++ ll.cpp
@@ -18,27 +18,20 @@ ll::ll(long long to): sign{ to < 0 }{
     num.push_back(to / power::mod);
 };
 ll::ll(std::string to){
-    std::string tmp_string = to.substr(to.length()-20, 21);
-    // do for() by 21 element strings while != endofstr
-    long long digit = std::stoll(to);
-    sign = (digit<0)? true:false;
-    if (sign) digit *= -1;
-    num.push_back(digit % power::mod);
-    digit /= power::mod;
-    num.push_back(digit % power::mod);
+    std::string tmp_string;
+    long long digit;
+    int len = to.length();
+    sign = (to[0]=='-')? true:false;
+    for(int i=18; len>0; i+=18){
+        tmp_string = (len > 18)? to.substr(len-18, 18):to.substr(sign, len-sign);
+        digit = std::stoll(tmp_string);
+        num.push_back(digit % power::mod);
+        if(len>18){
+            digit /= power::mod;
+            num.push_back(digit % power::mod);
+        }
+        len -= 18;
+    }
     num.push_back(digit / power::mod);
 };
 ll::ll(bool sigN, std::vector<long long> nuM): sign{ sigN }, num{ nuM }{};
-
-int main(void)
-{
-    // Just test for outputting
-    std::string digit_str = "-2233720368547758082";
-    ll digit = ll(digit_str);
-    if(digit.getSign()) std::cout << '-';
-    for(long long element: digit.getVec()){
-        std::cout << element;
-    }
-    // Just test for outputting
-    return 0;
-}
