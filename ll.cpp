ebb224e0156c85ebc92180c30aa1e8b8d64f2cfd
Ilya.Tsygankov <saw44947@gmail.com>
2021-04-16 18:15:49 +0300

    New constr for string

--- ll.cpp
+++ ll.cpp
@@ -18,20 +18,17 @@ ll::ll(long long to): sign{ to < 0 }{
     num.push_back(to / power::mod);
 };
 ll::ll(std::string to){
-    std::string tmp_string;
-    long long digit;
+    int digit = 0, degree = 1;
+    if(sign=(to[0]=='-')) to.erase(0,1);
     int len = to.length();
-    sign = (to[0]=='-')? true:false;
-    for(int i=18; len>0; i+=18){
-        tmp_string = (len > 18)? to.substr(len-18, 18):to.substr(sign, len-sign);
-        digit = std::stoll(tmp_string);
-        num.push_back(digit % power::mod);
-        if(len>18){
-            digit /= power::mod;
-            num.push_back(digit % power::mod);
+    for(int i=len-1; i>=0; i--){
+        digit = digit + ((int)to[i]-48)*degree;
+        degree *= 10;
+        if(!((len-i)%9)||!i){
+            num.push_back(digit);
+            digit = 0;
+            degree = 1;
         }
-        len -= 18;
     }
-    num.push_back(digit / power::mod);
 };
 ll::ll(bool sigN, std::vector<long long> nuM): sign{ sigN }, num{ nuM }{};
