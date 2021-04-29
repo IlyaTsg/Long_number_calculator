cf974eb52c5570804f65bc37b1cf33ad24decc8c
Artem Pinaev <74831426+MrBrain73@users.noreply.github.com>
2021-04-29 14:10:50 +0300

    Add comments

--- vln.cpp
+++ vln.cpp
@@ -1,6 +1,7 @@
 #include "vln.h"
 #include <iostream>
 
+//Пинаев Артём 0306
 vln::vln(): num{ 0 }{};
 
 vln::vln(int to){
@@ -121,6 +122,7 @@ bool operator <=(const T&  lval, const vln& rval)
     return lval.num <= rval.num;
 }
 
+//Пинаев Артём 0306
 vln operator +(const vln& lval, const vln& rval){
     vln temp = vln();
     temp.num = lval.num + rval.num;
@@ -153,6 +155,7 @@ vln operator -(const T&  lval, const vln& rval){
     return lval.num - rval.num;
 };
 
+//Новиков Владислав 0306
 char vln::com(const vln& secval) //N1
 {
     if (this->num > secval.num) return 2;
@@ -165,6 +168,7 @@ bool vln::nzer()//N2
     return num == 0;
 }
 
+//Пинаев Артём 0306
 vln vln::inc(){
     this->num = this->num + 1;
     return *this;
